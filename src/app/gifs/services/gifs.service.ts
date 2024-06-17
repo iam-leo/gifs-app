129041e5301c00a44cb2b011a08e18fb006926ab
Leo Ss <leo.vm.cba@gmail.com>
2024-06-17 20:46:54 -0300

    add: validations input search

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,8 +12,27 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      // Nuevo arreglo con los tags diferentes (sin duplicados)
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    //Inserto el tag del usuario
     this._tagsHistory.unshift(tag);
+
+    //Evitar que el arreglo contenga mas de 10 tags
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  public searchTag(tag: string): void {
+    // Validar input
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
     console.log(this._tagsHistory);
   }
 }
