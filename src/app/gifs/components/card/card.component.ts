5f730eee3cbc33c0c9865da6a6f9d1d130d6a064
Leo Ss <leo.vm.cba@gmail.com>
2024-06-20 19:01:58 -0300

    add: function to download gif

--- src/app/gifs/components/card/card.component.ts
+++ src/app/gifs/components/card/card.component.ts
@@ -13,4 +13,24 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
     if (!this.gif) throw new Error('Gif property is required');
   }
+
+  async downloadGif(url: string) {
+    try {
+      const response = await fetch(url, { mode: 'cors' });
+      const blob = await response.blob();
+      const downloadUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.download = this.getFileNameFromUrl(url);
+      link.click();
+      window.URL.revokeObjectURL(downloadUrl);
+    } catch (error) {
+      console.error('Error al descargar el GIF:', error);
+    }
+  }
+
+  private getFileNameFromUrl(url: string): string {
+    const fileName = url.split('/').pop();
+    return fileName ? fileName.split('?')[0] : 'download.gif';
+  }
 }
